6c46cf93ed709cc5421d2b70449607eca3ce32d6
enterprising <tanpeng_net@163.com>
2017-08-25 16:54:25 +0800

    gradle项目搭建完成

---
+++ src/main/java/Demo.java
@@ -0,0 +1,8 @@
+/**
+ * Created by peng.tan on 17/8/25.
+ */
+public class Demo {
+    public static void main(String[] args) {
+        System.out.println("hello world!");
+    }
+}
---
+++ src/main/java/net/peng/arithmetic/Demo.java
@@ -0,0 +1,7 @@
+package net.peng.arithmetic;
+
+/**
+ * Created by peng.tan on 17/8/25.
+ */
+public class Demo {
+}
