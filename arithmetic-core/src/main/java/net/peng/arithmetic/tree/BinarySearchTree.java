044976d059cdd507b08af0e0040cc3574ebf9076
enterprising <tanpeng_net@163.com>
2017-10-22 16:40:28 +0800

    bst

--- arithmetic-core/src/main/java/net/peng/arithmetic/tree/BinarySearchTree.java
+++ arithmetic-core/src/main/java/net/peng/arithmetic/tree/BinarySearchTree.java
@@ -71,10 +71,10 @@ public class BinarySearchTree {
     }
 
     /**
-     * 往二叉平衡树中插入数据
+     * 往二叉平衡树中插入数据 （插入简单，复杂的是删除操作）
      * <p>
      * 注意递归算法中有返回值，插入时是从上往下的查找，然后在树的底部插入，
-     * 然后在递归方法返回的过程中，是自下而上逐渐更新查找路径上的每个结点node的
+     * 然后在递归方法返回的过程中，是自下而上逐渐更新查找路径上的每个结点node的N
      *
      * @param node
      * @param key
@@ -103,4 +103,58 @@ public class BinarySearchTree {
         return node;
     }
 
+    private void remove(TreeNode node) {
+
+        TreeNode tmp;
+        if (node.left == null && node.right == null) {
+            // 当前node没有子节点，这时候直接删除当前节点就行了
+            node = null;
+        } else if (node.left == null && node.right != null) {
+            // 没有左子树，右子树直接上去
+            tmp = node;
+            node = node.right;
+            free(tmp);
+        } else if (node.left != null && node.right == null) {
+            // 没有右子树，左子树直接上去
+            tmp = node;
+            node = node.left;
+            free(tmp);
+        } else {
+            // 最复杂的是这里，左右子树都有。这里可能需要调整
+            // 找到离当前node节点对应元素且最近的节点 target（左子树的最右节点，或者右子树最左节点）
+            // 将node节点元素替换成target元素的节点，将target节点删除
+            tmp = node;  //tmp是node的父节点
+            TreeNode target = node.left; //找左子树的最大节点
+            while (target.right != null) {
+                tmp = target;
+                target = target.right;
+            }
+            // 复制节点的内容
+            node.key = target.key;
+            node.value = target.value;
+            node.left = target.left;
+            node.right = target.right;
+            if (tmp.key == node.key) {
+                // tmp == node 说明没有在左子树中进行右拐,也就是node节点的左孩子没有右孩子,
+                // 需要重新连接tmp节点左孩子
+                tmp.left = target.left;
+            } else {
+                // tmp != node, 进行了右拐,那么将重新连接tmp的右子树,将target.lChild赋值给tmp.rChild
+                tmp.right = target.left;
+            }
+            free(target);
+        }
+
+    }
+
+    /**
+     * 释放当前节点
+     *
+     * @param node
+     */
+    private void free(TreeNode node) {
+        node.left = null;
+        node.right = null;
+        node = null;
+    }
 }
