9dec804a5cc15b25637623271bf4d2dc58cde891
enterprising <tanpeng_net@163.com>
2017-10-26 10:23:06 +0800

    change sth

--- arithmetic-core/src/main/java/database/SelectDemo.java
+++ arithmetic-core/src/main/java/database/SelectDemo.java
@@ -9,18 +9,18 @@ public class SelectDemo {
     public static void main(String[] args) throws SQLException {
         String serverName = "127.0.0.1";
         int port = 3306;
-        String databaseName = "daltestdb";
-        String dbname = "restaurant";
+        String databaseName = "tanpeng";
+        String dbname = "test";
         String url = String.format("jdbc:mysql://%s:%d/%s", serverName, port, databaseName);
         String username = "root";
         String password = "";
 
-        String sql = String.format("select count(*) from %s", dbname);
+        String sql = String.format("select * from %s", dbname);
 
         Connection connection = DriverManager.getConnection(url, username, password);
         Statement statement = connection.createStatement();
         ResultSet resultSet = statement.executeQuery(sql);
-        if (resultSet.next()){
+        while (resultSet.next()) {
             System.out.println(resultSet.getString(1));
         }
 
