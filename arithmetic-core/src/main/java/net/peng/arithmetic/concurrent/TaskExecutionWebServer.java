3c48f66f075f05b90bde401209ad4306f7805e35
enterprising <tanpeng_net@163.com>
2017-10-27 09:48:15 +0800

    Executor.newFixedThreadPoll(xxx)

---
+++ arithmetic-core/src/main/java/net/peng/arithmetic/concurrent/TaskExecutionWebServer.java
@@ -0,0 +1,34 @@
+package net.peng.arithmetic.concurrent;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+
+/**
+ * 基于线程池的 web 服务器，用Executor框架
+ */
+public class TaskExecutionWebServer {
+    private static final int NTHREADS = 100;
+
+    /**
+     * 注：alibaba建议手动创建连接池，用ThreadPoolExecutor
+     */
+    private static final Executor exec = Executors.newFixedThreadPool(NTHREADS);
+
+    public static void main(String[] args) throws IOException {
+        ServerSocket socket = new ServerSocket(80);
+        while (true) {
+            final Socket connection = socket.accept();
+            Runnable task = new Runnable() {
+                @Override
+                public void run() {
+                    // do sth
+                }
+            };
+            exec.execute(task);
+        }
+    }
+
+}
