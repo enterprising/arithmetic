998ba2746f792b252d8b9596b8057672384ef330
enterprising <tanpeng_net@163.com>
2017-11-09 10:34:34 +0800

    LeetCode654 典型递归问题

---
+++ arithmetic-core/src/main/java/leetcode/MediumT654.java
@@ -0,0 +1,47 @@
+package leetcode;
+
+/**
+ * Maximum Binary Tree
+ */
+
+/**
+ * Definition for a binary tree node.
+ */
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+        val = x;
+    }
+}
+
+public class MediumT654 {
+
+    // 思路，左右递归。 重点是找到递归出口以及递归条件
+    public TreeNode constructMaximumBinaryTree(int[] nums) {
+        if (nums == null) {
+            return null;
+        }
+        return build(nums, 0, nums.length - 1);
+    }
+
+    private TreeNode build(int[] nums, int start, int end) {
+        if (start > end) {
+            return null;
+        }
+        int maxIndex = start;
+        for (int i = start + 1; i <= end; i++) {
+            if (nums[i] > nums[maxIndex]) {
+                maxIndex = i;
+            }
+        }
+
+        TreeNode root = new TreeNode(nums[maxIndex]);
+        root.left = build(nums, start, maxIndex - 1);
+        root.right = build(nums, maxIndex + 1, end);
+
+        return root;
+    }
+}
