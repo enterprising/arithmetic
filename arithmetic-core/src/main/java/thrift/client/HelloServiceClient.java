ce1935f47bc7c593f214d1d717d9b482e63814b0
enterprising <tanpeng_net@163.com>
2017-11-19 21:59:19 +0800

    add thrift pom

--- arithmetic-core/src/main/java/thrift/client/HelloServiceClient.java
+++ arithmetic-core/src/main/java/thrift/client/HelloServiceClient.java
@@ -1,5 +1,11 @@
 package thrift.client;
 
+import org.apache.thrift.TException;
+import org.apache.thrift.protocol.TBinaryProtocol;
+import org.apache.thrift.protocol.TProtocol;
+import org.apache.thrift.transport.TSocket;
+import org.apache.thrift.transport.TTransport;
+import org.apache.thrift.transport.TTransportException;
 import thrift.demo.Hello;
 
 public class HelloServiceClient {
--- arithmetic-core/src/main/java/thrift/server/HelloServiceServer.java
+++ arithmetic-core/src/main/java/thrift/server/HelloServiceServer.java
@@ -1,5 +1,11 @@
 package thrift.server;
 
+import org.apache.thrift.TProcessor;
+import org.apache.thrift.protocol.TBinaryProtocol;
+import org.apache.thrift.server.TServer;
+import org.apache.thrift.server.TThreadPoolServer;
+import org.apache.thrift.transport.TServerSocket;
+import org.apache.thrift.transport.TTransportException;
 import thrift.demo.Hello;
 import thrift.demo.HelloServiceImpl;
 
@@ -14,7 +20,7 @@ public class HelloServiceServer {
             // 设置服务端口为 7911
             TServerSocket serverTransport = new TServerSocket(7911);
             // 设置协议工厂为 TBinaryProtocol.Factory
-            Factory proFactory = new TBinaryProtocol.Factory();
+            TBinaryProtocol.Factory proFactory = new TBinaryProtocol.Factory();
             // 关联处理器与 Hello 服务的实现
             TProcessor processor = new Hello.Processor(new HelloServiceImpl());
             TServer server = new TThreadPoolServer(processor, serverTransport,
