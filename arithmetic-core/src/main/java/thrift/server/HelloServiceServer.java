56cae238ce9a3eda5a323d2832bd3c07d5159130
enterprising <tanpeng_net@163.com>
2017-11-29 20:24:13 +0800

    注释一些代码

--- arithmetic-core/src/main/java/thrift/server/HelloServiceServer.java
+++ arithmetic-core/src/main/java/thrift/server/HelloServiceServer.java
@@ -23,10 +23,10 @@ public class HelloServiceServer {
             TBinaryProtocol.Factory proFactory = new TBinaryProtocol.Factory();
             // 关联处理器与 Hello 服务的实现
             TProcessor processor = new Hello.Processor(new HelloServiceImpl());
-            TServer server = new TThreadPoolServer(processor, serverTransport,
-                    proFactory);
-            System.out.println("Start server on port 7911...");
-            server.serve();
+//            TServer server = new TThreadPoolServer(processor, serverTransport,
+//                    proFactory);   这个方法已经无效
+//            System.out.println("Start server on port 7911...");
+//            server.serve();
         } catch (TTransportException e) {
             e.printStackTrace();
         }
