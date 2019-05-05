85e5f262a0561b396fb02fc719054cac34e8db19
enterprising <tanpeng.net@gmail.com>
2019-05-05 15:56:50 +0800

    add leetcode T21

---
+++ arithmetic-leetcode/src/main/java/net/tanpeng/arithmetic/leetcode/T21.java
@@ -0,0 +1,65 @@
+package net.tanpeng.arithmetic.leetcode;
+
+/**
+ * https://leetcode.com/problems/merge-two-sorted-lists/description/
+ * <p>
+ * Created by peng.tan on 2019/5/5.
+ */
+public class T21 {
+
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+    }
+
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        // 归并排序里面有个很重要的环节就是合并两个有序的数组，这里就是这个
+        if (l1 == null && l2 == null) {
+            return null;
+        }
+        if (l1 == null) {
+            return l2;
+        }
+        if (l2 == null) {
+            return l1;
+        }
+        ListNode result = new ListNode(0);
+        ListNode prev = result;
+        while (l1 != null && l2 != null) {
+            if (l1.val <= l2.val) {
+                prev.next = l1;
+                l1 = l1.next;
+            } else {
+                prev.next = l2;
+                l2 = l2.next;
+            }
+            prev = prev.next;
+        }
+        if (l1 != null) {
+            prev.next = l1;
+        }
+        if (l2 != null) {
+            prev.next = l2;
+        }
+
+        return result.next;
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
