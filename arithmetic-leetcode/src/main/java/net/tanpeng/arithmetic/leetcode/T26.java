6381fa464e5cc941b0c8af2de91cb07907eb90c4
enterprising <tanpeng.net@gmail.com>
2019-06-12 16:19:04 +0800

    leetcode easy T26

--- arithmetic-leetcode/src/main/java/net/tanpeng/arithmetic/leetcode/T26.java
+++ arithmetic-leetcode/src/main/java/net/tanpeng/arithmetic/leetcode/T26.java
@@ -1,7 +1,28 @@
 package net.tanpeng.arithmetic.leetcode;
 
+import java.util.Set;
+import java.util.TreeSet;
+
 /**
  * Created by peng.tan on 2019/5/8.
  */
 public class T26 {
+    private int removeDuplicates(int[] nums) {
+        int index = 0;
+        Set<Integer> result = new TreeSet<>();
+        for (int num : nums) {
+            result.add(num);
+        }
+        for (Integer i : result) {
+            nums[index++] = i;
+        }
+        return result.size();
+    }
+
+    public static void main(String[] args) {
+        T26 t26 = new T26();
+        t26.removeDuplicates(new int[]{1, 1, 2});
+    }
+
+
 }
