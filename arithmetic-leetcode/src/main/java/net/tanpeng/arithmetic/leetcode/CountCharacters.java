614cd0ccc0f5b3b271d2e3f2c869d6fccc5d6017
enterprising <tanpeng.net@gmail.com>
2020-03-17 23:40:09 +0800

    update leetcode

---
+++ arithmetic-leetcode/src/main/java/net/tanpeng/arithmetic/leetcode/CountCharacters.java
@@ -0,0 +1,41 @@
+package net.tanpeng.arithmetic.leetcode;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class CountCharacters {
+    public static int countCharacters(String[] words, String chars) {
+        Map<Character, Integer> map = new HashMap<>();
+        for (char c : chars.toCharArray()) {
+            map.compute(c, (k, v) -> (v == null) ? 1 : v + 1);
+        }
+        int result = 0;
+        for (String word : words) {
+            Map<Character, Integer> tempMap = new HashMap<>();
+            for (char c : word.toCharArray()) {
+                tempMap.compute(c, (k, v) -> (v == null) ? 1 : v + 1);
+            }
+            boolean flag = true;
+            for (Map.Entry<Character, Integer> entry : tempMap.entrySet()) {
+                Character k = entry.getKey();
+                Integer v = entry.getValue();
+                if (!map.containsKey(k)) {
+                    flag = false;
+                    break;
+                }
+                if (v > map.get(k)) {
+                    flag = false;
+                    break;
+                }
+            }
+            if (flag) {
+                result += word.length();
+            }
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        countCharacters(new String[2], "xasdsaq");
+    }
+}
---
+++ arithmetic-leetcode/src/main/java/net/tanpeng/arithmetic/leetcode/DiameterOfBinaryTree.java
@@ -0,0 +1,34 @@
+package net.tanpeng.arithmetic.leetcode;
+
+public class DiameterOfBinaryTree {
+    private static int max = 0;
+
+    public int diameterOfBinaryTree(TreeNode root) {
+        max = 1;
+        depth(root);
+        return max - 1;
+    }
+
+    /**
+     * 深度遍历，加一层判断
+     *
+     * @param root
+     */
+    private int depth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int left = depth(root.left);  // 1、左
+        int right = depth(root.right);  // 2、右
+        max = left + right + 1 > max ? left + right + 1 : max; // 4、更新最大值
+        return Math.max(left, right) + 1;  //  3、深度
+    }
+
+
+    public static void main(String[] args) {
+        // 我的思路是左右递归
+        TreeNode tree = new TreeNode(1);
+
+    }
+
+}
