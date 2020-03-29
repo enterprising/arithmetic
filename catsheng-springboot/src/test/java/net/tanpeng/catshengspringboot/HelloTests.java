a3daa847ed8edc88ad4492fd539158a692e860df
enterprising <tanpeng.net@gmail.com>
2020-03-30 00:10:11 +0800

    springboot helloworld

---
+++ catsheng-springboot/src/test/java/net/tanpeng/catshengspringboot/HelloTests.java
@@ -0,0 +1,43 @@
+package net.tanpeng.catshengspringboot;
+
+import net.tanpeng.catshengspringboot.controller.HelloWorldController;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+/**
+ * @author: peng.tan
+ * @create: 2020/03/30 00:04
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HelloTests {
+
+
+    private MockMvc mvc;
+
+    @Before
+    public void setUp() throws Exception {
+        mvc = MockMvcBuilders.standaloneSetup(new HelloWorldController()).build();
+    }
+
+    @Test
+    public void getHello() throws Exception {
+        mvc.perform(MockMvcRequestBuilders.get("/hello").accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().string(equalTo("Hello World")));
+        MockMvcResultHandlers.print();
+    }
+
+}
