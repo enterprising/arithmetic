867898d1b1c59f0070df7b6c14a7bf2af86a4a46
enterprising <tanpeng.net@gmail.com>
2020-05-02 16:07:51 +0800

    add explain

--- catsheng-springboot/src/main/java/net/tanpeng/catshengspringboot/controller/HelloWorldController.java
+++ catsheng-springboot/src/main/java/net/tanpeng/catshengspringboot/controller/HelloWorldController.java
@@ -4,6 +4,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
+ * http://localhost:8080/hello
+ *
  * @author: peng.tan
  * @create: 2020/03/29 23:53
  * RestController 注解的意思就是 Controller 里面的方法都以 json 格式输出，不用再写什么 jackjson 配置的了
