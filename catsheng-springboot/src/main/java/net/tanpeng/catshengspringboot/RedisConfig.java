9bb92f944f66e9ca1720b484a1268beabb0ca78f
enterprising <tanpeng.net@gmail.com>
2020-05-24 00:26:31 +0800

    springboot redis

---
+++ catsheng-springboot/src/main/java/net/tanpeng/catshengspringboot/RedisConfig.java
@@ -0,0 +1,35 @@
+package net.tanpeng.catshengspringboot;
+
+import org.springframework.cache.annotation.CachingConfigurerSupport;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.interceptor.KeyGenerator;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.lang.reflect.Method;
+
+/**
+ * @EnableCaching 用来开启缓存
+ *
+ * @author: peng.tan
+ * @create: 2020/05/23 22:58
+ */
+@Configuration
+@EnableCaching
+public class RedisConfig extends CachingConfigurerSupport {
+    @Bean
+    public KeyGenerator keyGenerator() {
+        return new KeyGenerator() {
+            @Override
+            public Object generate(Object target, Method method, Object... params) {
+                StringBuilder sb = new StringBuilder();
+                sb.append(target.getClass().getName());
+                sb.append(method.getName());
+                for (Object obj : params) {
+                    sb.append(obj.toString());
+                }
+                return sb.toString();
+            }
+        };
+    }
+}
---
+++ catsheng-springboot/src/main/java/net/tanpeng/catshengspringboot/redis/TestRedis.java
@@ -0,0 +1,9 @@
+package net.tanpeng.catshengspringboot.redis;
+
+/**
+ * 微笑的demo没跑起来，详细文档参考：https://juejin.im/post/5d8d5c1b6fb9a04df85a01f2#heading-11
+ * @author: peng.tan
+ * @create: 2020/05/23 22:59
+ */
+public class TestRedis {
+}
