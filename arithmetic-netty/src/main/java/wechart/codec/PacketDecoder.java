d8895857b9a71338d974e96f7ed4d4804c23a250
enterprising <tanpeng.net@gmail.com>
2020-07-06 23:40:21 +0800

    clean code

--- arithmetic-netty/src/main/java/wechart/codec/PacketDecoder.java
+++ arithmetic-netty/src/main/java/wechart/codec/PacketDecoder.java
@@ -8,6 +8,8 @@ import wechart.protocol.PacketCodeC;
 import java.util.List;
 
 /**
+ * 客户端用decoder，主要是把服务端传过来的字节流转成这边认识的 包对象
+ *
  * Created by peng.tan on 2019/5/2.
  */
 public class PacketDecoder extends MessageToMessageDecoder<ByteBuf> {
