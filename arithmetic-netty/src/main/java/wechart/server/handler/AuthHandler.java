eafd1db685334fae50c8d911b38e03f856826c18
enterprising <tanpeng.net@gmail.com>
2020-07-13 23:33:56 +0800

    handler 热拔插

--- arithmetic-netty/src/main/java/wechart/server/handler/AuthHandler.java
+++ arithmetic-netty/src/main/java/wechart/server/handler/AuthHandler.java
@@ -6,6 +6,9 @@ import io.netty.channel.ChannelInboundHandlerAdapter;
 import wechart.util.SessionUtil;
 
 /**
+ *
+ * AuthHandler 继承自 ChannelInboundHandlerAdapter，覆盖了 channelRead() 方法，表明他可以处理所有类型的数据
+ *
  * Created by peng.tan on 2019/5/4.
  */
 @ChannelHandler.Sharable
@@ -17,6 +20,8 @@ public class AuthHandler extends ChannelInboundHandlerAdapter {
         if (!SessionUtil.hasLogin(ctx.channel())) {
             ctx.channel().close();
         } else {
+            // 如果某一个独立的逻辑在执行几次之后（这里是一次）不需要再执行了，
+            // 那么我们可以通过 ChannelHandler 的热插拔机制来实现动态删除逻辑，应用程序性能处理更为高效
             ctx.pipeline().remove(this);
             super.channelRead(ctx, msg);
         }
