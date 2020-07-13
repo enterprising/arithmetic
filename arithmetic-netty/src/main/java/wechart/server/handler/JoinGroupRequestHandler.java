3e78116d028e66c1e45bd4e58026c60d91b8ae39
enterprising <tanpeng.net@gmail.com>
2020-07-13 23:57:35 +0800

    group netty wechart

--- arithmetic-netty/src/main/java/wechart/server/handler/JoinGroupRequestHandler.java
+++ arithmetic-netty/src/main/java/wechart/server/handler/JoinGroupRequestHandler.java
@@ -9,6 +9,11 @@ import wechart.protocol.response.JoinGroupResponsePacket;
 import wechart.util.SessionUtil;
 
 /**
+ * 首先，我们这里创建一个 ChannelGroup。这里简单介绍一下 ChannelGroup：它可以把多个 chanel 的操作聚合在一起，可以往它里面添加删除 channel，可以进行 channel 的批量读写，关闭等操作，详细的功能读者可以自行翻看这个接口的方法。这里我们一个群组其实就是一个 channel 的分组集合，使用 ChannelGroup 非常方便。
+ * 接下来，我们遍历待加入群聊的 userId，如果存在该用户，就把对应的 channel 添加到 ChannelGroup 中，用户昵称也添加到昵称列表中。
+ * 然后，我们创建一个创建群聊响应的对象，其中 groupId 是随机生成的，群聊创建结果一共三个字段，这里就不展开对这个类进行说明了。
+ * 最后，我们调用 ChannelGroup 的聚合发送功能，将拉群的通知批量地发送到客户端，接着在服务端控制台打印创建群聊成功的信息，至此，服务端处理创建群聊请求的逻辑结束。
+ *
  * Created by peng.tan on 2019/5/5.
  */
 @ChannelHandler.Sharable
