97b42fec72f33fda43f38e9f38290cbb9386f9c7
enterprising <tanpeng.net@gmail.com>
2020-08-16 18:20:04 +0800

    二分查找

--- arithmetic-leetcode/src/main/java/net/tanpeng/arithmetic/ememiddle/TwoSplit.java
+++ arithmetic-leetcode/src/main/java/net/tanpeng/arithmetic/ememiddle/TwoSplit.java
@@ -8,22 +8,20 @@ package net.tanpeng.arithmetic.ememiddle;
 public class TwoSplit {
     public static void main(String[] args) {
         int[] a = new int[]{1, 2, 2, 3, 4, 5, 9, 11};
-        int target = 10;
+        int target = 9;
 
         int low = 0;
         int high = a.length - 1;
-        int mid = low / 2 + high / 2;
-
         while (low < high) {
+            int mid = low + (high - low) / 2;
             if (a[mid] > target) {
                 high = mid - 1;
             } else if (a[mid] < target) {
                 low = mid + 1;
             } else {
-                System.out.println("在下标：" + mid + "  :" + a[mid]);
-                break;
+                System.out.println("在下标：" + mid + " : " + a[mid]);
+                return;
             }
-            mid = low / 2 + high / 2;
         }
         System.out.println("不存在");
     }
