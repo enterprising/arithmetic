254a0cf9c800a43cc0e4e8eec29fa64462c7d756
enterprising <tanpeng.net@gmail.com>
2021-03-13 23:23:12 +0800

    hash set

---
+++ arithmetic-leetcode/src/main/java/net/tanpeng/arithmetic/leetcode/MyHashSet.java
@@ -0,0 +1,76 @@
+package net.tanpeng.arithmetic.leetcode;
+
+import java.util.Iterator;
+import java.util.LinkedList;
+
+/**
+ * 实现简单的hash set，https://leetcode-cn.com/problems/design-hashset/solution/she-ji-ha-xi-ji-he-by-leetcode-solution-xp4t/
+ * @author: peng.tan
+ * @create: 2021/03/13 22:45
+ */
+
+public class MyHashSet {
+    private static final int BASE = 769;
+    private LinkedList[] data;
+
+    /**
+     * Initialize your data structure here.
+     */
+    public MyHashSet() {
+        data = new LinkedList[BASE];
+        for (int i = 0; i < BASE; ++i) {
+            data[i] = new LinkedList<Integer>();
+        }
+    }
+
+    public void add(int key) {
+        int h = hash(key);
+        Iterator<Integer> iterator = data[h].iterator();
+        while (iterator.hasNext()) {
+            Integer element = iterator.next();
+            if (element == key) {
+                return;
+            }
+        }
+        data[h].offerLast(key);
+    }
+
+    public void remove(int key) {
+        int h = hash(key);
+        Iterator<Integer> iterator = data[h].iterator();
+        while (iterator.hasNext()) {
+            Integer element = iterator.next();
+            if (element == key) {
+                data[h].remove(element);
+                return;
+            }
+        }
+    }
+
+    /**
+     * Returns true if this set contains the specified element
+     */
+    public boolean contains(int key) {
+        int h = hash(key);
+        Iterator<Integer> iterator = data[h].iterator();
+        while (iterator.hasNext()) {
+            Integer element = iterator.next();
+            if (element == key) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private static int hash(int key) {
+        return key % BASE;
+    }
+}
+
+/**
+ * Your MyHashSet object will be instantiated and called as such:
+ * MyHashSet obj = new MyHashSet();
+ * obj.add(key);
+ * obj.remove(key);
+ * boolean param_3 = obj.contains(key);
+ */
