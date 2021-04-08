e014b645c3c6caabc32c267b56e8128b1a135a28
enterprising <tanpeng.net@gmail.com>
2021-04-08 23:51:54 +0800

    bfs demo

---
+++ arithmetic-leetcode/src/main/java/net/tanpeng/arithmetic/offers/BFSDemo.java
@@ -0,0 +1,36 @@
+package net.tanpeng.arithmetic.offers;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * T111 二叉树的最小高度，套用bfs框架，从起点找终点
+ */
+public class BFSDemo {
+    int minDepth(TreeNode root) {
+        if (root == null) return 0;
+        Queue<TreeNode> q = new LinkedList<>();
+        q.offer(root);
+        // root 本身就是一层，depth 初始化为 1
+        int depth = 1;
+
+        while (!q.isEmpty()) {
+            int sz = q.size();
+            /* 将当前队列中的所有节点向四周扩散 */
+            for (int i = 0; i < sz; i++) {
+                TreeNode cur = q.poll();
+                /* 判断是否到达终点 */
+                if (cur.left == null && cur.right == null)
+                    return depth;
+                /* 将 cur 的相邻节点加入队列 */
+                if (cur.left != null)
+                    q.offer(cur.left);
+                if (cur.right != null)
+                    q.offer(cur.right);
+            }
+            /* 这里增加步数 */
+            depth++;
+        }
+        return depth;
+    }
+}
